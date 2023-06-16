8bc7e423ae85b8ed5c5721c2bbd70e48491b5220
SoumyaNayak2000 <128832907+SoumyaNayak2000@users.noreply.github.com>
2023-06-17 01:21:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
+        const relatives = ["Relative 1", "Relative 2", "Relative 3", "Relative 4"];
 
         return(
             <div id="main">
                {/* Do not remove the main div */}
+             <ol key="relativeList">
+                    {relatives.map((relative, index) => (
+                        <li key={`relativeListItem${index + 1}`}>{relative}</li>
+                    ))}
+                </ol>
             </div>
         )
     }
